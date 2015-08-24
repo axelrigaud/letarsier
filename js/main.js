01b9a56fec0b1380f85495c54798923be57f6e44
Axel Rigaud <axel.rigaud@jobteaser.com>
2015-08-24 12:19:26 +0200

    loader

--- js/main.js
+++ js/main.js
@@ -72,31 +72,9 @@ $(document).ready(function(){
     //$ytplayer.YTPPlay();
   })
 
-  $('.hero').animsition({
-  
-    inClass               :   'fade-in',
-    outClass              :   'fade-out',
-    inDuration            :    1500,
-    outDuration           :    800,
-    linkElement           :   '.animsition-link',
-    // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
-    loading               :    true,
-    loadingParentElement  :   'body', //animsition wrapper element
-    loadingClass          :   'animsition-loading',
-    unSupportCss          : [ 'animation-duration',
-                              '-webkit-animation-duration',
-                              '-o-animation-duration'
-                            ],
-    //"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-    //The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    
-    overlay               :   false,
-    
-    overlayClass          :   'animsition-overlay-slide',
-    overlayParentElement  :   'body'
+  $(window).on("load", function(){
+    $('.loader-wrapper').fadeOut(700);
+    $('.hero').fadeIn(700);
   });
-
-
- 
 
 });
