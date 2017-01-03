c6d29d838447ac553a84f5e0fc231481145e416a
axel rigaud <axel.rigaud@gmail.com>
2017-01-04 00:20:14 +0100

    concerts'

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -2,22 +2,52 @@
 
 $(document).ready(function () {
 
-  var fadeTime = 700;
-  $(window).on('load', function () {
-    $('.loader-wrapper').fadeOut(fadeTime);
-    $('.hero').fadeIn(fadeTime, function () {});
-  });
-
-  //ga
-  $('a').on('click', function (e) {
-    trackOutboundLink($(this).attr('href'));
-  });
-
-  var trackOutboundLink = function trackOutboundLink(url) {
-    ga('send', 'event', 'outbound', 'click', url, { 'hitCallback': function hitCallback() {
-        document.location = url;
+  var Concerts = function () {
+    var buildTable = function buildTable() {
+      var items = [];
+      $.getJSON("/concerts.json", function (data) {
+        $.each(data.concerts, function (key, val) {
+          items.push('<tr class="gigs__row"><td class="gigs__date">' + val.date + '</td><td class="gigs__location">' + val.location + '</td><td class="gigs__description">' + val.details + '</td></tr>');
+        });
+        $("<tbody/>", {
+          html: items.join("")
+        }).appendTo(".gigs__table");
+      });
+    };
+    return {
+      init: function init() {
+        buildTable();
       }
-    });
-  };
+    };
+  }();
+
+  var App = function () {
+
+    var fadeTime = 700;
+    var trackOutboundLink = function trackOutboundLink(url) {
+      ga('send', 'event', 'outbound', 'click', url, { 'hitCallback': function hitCallback() {
+          document.location = url;
+        }
+      });
+    };
+
+    return {
+      init: function init() {
+
+        $(window).on('load', function () {
+          $('.loader-wrapper').fadeOut(fadeTime);
+          $('.hero').fadeIn(fadeTime, function () {});
+        });
+
+        $('a').on('click', function (e) {
+          trackOutboundLink($(this).attr('href'));
+        });
+
+        Concerts.init();
+      }
+    };
+  }();
+
+  App.init();
 });
 //# sourceMappingURL=main.js.map
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,64 @@
 $(document).ready(function(){
 
-  var fadeTime = 700;
-  $(window).on('load', function(){
-    $('.loader-wrapper').fadeOut(fadeTime);
-    $('.hero').fadeIn(fadeTime, function(){
-
-    });
-  });
-
-  //ga
-  $('a').on('click', function(e){
-    trackOutboundLink($(this).attr('href'));
-  });
-
-  var trackOutboundLink = function(url) {
-    ga('send', 'event', 'outbound', 'click', url, {'hitCallback':
-      function () {
-      document.location = url;
+  var Concerts = (function(){
+    var buildTable = function () {
+      var items = [];
+      $.getJSON("/concerts.json", function (data) {
+        $.each( data.concerts, function (key, val) {
+          items.push('<tr class="gigs__row">
+                        <td class="gigs__date">'+val.date+'</td>
+                        <td class="gigs__location">'+val.location+'</td>
+                        <td class="gigs__description">'+val.details+'</td>
+                      </tr>');
+        });
+        $( "<tbody/>", {
+          html: items.join( "" )
+        }).appendTo( ".gigs__table" );
+      });
+    }
+    return {
+      init: function () {
+        buildTable();
       }
-    });
-  }
+    }
+  })();
+
+  var App = (function(){
+
+    var fadeTime = 700;
+    var trackOutboundLink = function(url) {
+      ga('send', 'event', 'outbound', 'click', url, {'hitCallback':
+        function () {
+        document.location = url;
+        }
+      });
+    }
+
+    return {
+      init: function () {
+
+        $(window).on('load', function() {
+          $('.loader-wrapper').fadeOut(fadeTime);
+          $('.hero').fadeIn(fadeTime, function(){
+          });
+        });
+
+        $('a').on('click', function(e){
+          trackOutboundLink($(this).attr('href'));
+        });
+
+        Concerts.init();
+
+      }
+    }
+  })();
+
+  App.init();
+
+  $('.hero').scroll(function() {
+    console.log('scroll s')
+      var y = $(this).scrollTop();
+      $(this).css('background-position', 'center ' + parseInt(-y / 10) + 'px');
+  });
 
 });
